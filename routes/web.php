2a284e719c5e86cfcfafd4279194f957926c3f69
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2020-05-18 17:29:31 +0700

    Routes dan file2 basic

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/about', function () {
+//     return view('pages.about');
+// });
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
